7cb8e5283bb34b3cc1c15e4d0177cc299dce7dfa
Heidi Ahlgren <heidiahlgren@hotmail.com>
2024-11-15 09:31:17 +0200

    css added to the frontpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-
+import './tabsStyles.css';
 
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -35,19 +35,24 @@ function App() {
         justifyContent="center"
         alignItems="center"
       >
-        <AppBar position="fixed">
+        <AppBar position="fixed" sx={{ backgroundColor: '#40A056' }}> 
           <Toolbar>
             <Typography variant="h6">Ompun ja Ranen kauppa</Typography>
           </Toolbar>
         </AppBar>
 
-
+        <img src="/src/images/Paw_Print.png" alt="Paw" style={{ width: '250px', height: 'auto', margin: '20px 0' }}/>
 
         {/*TABCONTEXT, TABS and TABPANEL implemented with help by MUI docs and CHATGPT */}
         <TabContext value={value}>
           <Tabs
             value={value} onChange={handleChange}
-            centered>
+            centered
+            sx={{
+              width:"100%",
+              backgroundColor: '#40A056'
+            }}
+            >
 
             <Tab label="HOME PAGE" value="0" />
             <Tab label="PRODUCTS" value="1" />
@@ -58,7 +63,7 @@ function App() {
           <TabPanel value="0">
 
 
-            <p>Welcome to the app!</p>
+            <p>Welcome to the shop!</p>
           </TabPanel>
 
           <TabPanel value="1">
