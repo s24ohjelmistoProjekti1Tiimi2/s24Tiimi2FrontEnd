bef8050d796a8317fca17d337b42c132d1a62f50
Tiriele <147039063+Tiriele@users.noreply.github.com>
2024-11-27 12:01:57 +0200

    Welcome-text modified

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,16 @@
-import { Stack, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import '../App.css'
 
 export default function Home() {
 
     return(
-        <Stack>
+        <Box style={{ marginTop: '-400px', marginLeft: '-350px' }}>
+
         <Typography variant='h2' style={{color: 'black'}}>Welcome to Omppu and Rane's shop</Typography>
+
+        <Typography style={{}}>Here you will find the latest and most popular dog supplies and food for your furry friend!</Typography>
         
-        </Stack>
+        </Box>
     )
     
 }
