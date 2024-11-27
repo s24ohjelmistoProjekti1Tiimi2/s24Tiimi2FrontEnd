09700df07aa7c7d5f810d0832e7e2bb4dcb4562e
Heidi Ahlgren <heidiahlgren@hotmail.com>
2024-11-27 11:47:16 +0200

    home text updated and favicon addded

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,13 @@
+import { Stack, Typography } from '@mui/material'
 import '../App.css'
 
 export default function Home() {
 
-    return(<h1>This is home page</h1>)
+    return(
+        <Stack>
+        <Typography variant='h2' style={{color: 'black'}}>Welcome to Omppu and Rane's shop</Typography>
+        
+        </Stack>
+    )
     
 }
