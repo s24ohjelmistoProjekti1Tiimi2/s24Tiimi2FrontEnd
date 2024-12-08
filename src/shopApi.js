9a729afaba5995da64e4d127e2af9ded32748d72
Netta <bhi084@myy.haaga-helia.fi>
2024-12-08 23:07:33 +0200

    GetCustomerId working now

--- src/shopApi.js
+++ src/shopApi.js
@@ -20,17 +20,23 @@ export function saveRegistration(formData) {
 }
 
 export function getCustomerId(email) {
-  return fetch(import.meta.env.VITE_API_PRODUCTS_URL)
+  return fetch(import.meta.env.VITE_API_REGISTRATION_URL)
   .then((response) => {
-    const customers = response.json();
-    customers.map((customer, index) => {
-      customer.id = index;
-      if (customer.email === email) {
-        return index;
+    if (!response.ok) {
+      throw new Error("Failed to fetch customers: " + response.statusText);
+    }
+    return response.json();
+  })
+  .then((customers) => {
+    const array = customers;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].email === email) {
+        const customerId = array[i].id;
+        return customerId;
       }
-    })
-    .catch((err) => console.error(err));
-  });
+    }
+  })
+  .catch((err) => console.error(err));
 }
 
 export function softDeleteCustomer(customerId) {
@@ -38,7 +44,7 @@ export function softDeleteCustomer(customerId) {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      deleted: "true"
+      deleted: true
     }),
   }).then((response) => {
     if (!response.ok)
