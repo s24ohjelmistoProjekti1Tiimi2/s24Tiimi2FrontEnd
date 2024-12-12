7803601abca2d4ba4ff3c203b194dec5c77cf68b
Heidi Ahlgren <heidiahlgren@hotmail.com>
2024-12-12 13:46:30 +0200

    package.json and vite.config.js updated for netlify

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/s24Tiimi2FrontEnd/',
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
